194fb7955db4c19625c4bbb7d570fea6dc21e47d
SmileRexar <53871193+SmileRexar@users.noreply.github.com>
2020-03-14 17:08:27 -0400

    fix request. Add collection artists with full arg

--- SimpleApi/basic_Controller/ArtsyHelper.py
+++ SimpleApi/basic_Controller/ArtsyHelper.py
@@ -2,6 +2,8 @@ import requests
 import json
 
 # eyt67259@zzrgg.com
+from SimpleApi.basic_Controller.Weather import pretty_print_request
+
 client_id = '2ed42f52ed8a8dc9bc7e'
 client_secret = '6ec542deca8bc6b22bf46effd9ff2d83'
 
@@ -17,3 +19,14 @@ j = json.loads(r.text)
 
 # токен
 token = j["token"]
+# создать заголовок содераи Header
+headers = {"X-Xapp-Token": token}
+
+with open('dataset_24476_4.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        # инииируем запрос с заголовком
+        r = requests.get(f"https://api.artsy.net/api/artists/{line.strip()}", headers=headers)
+
+        # разбор ответа сервера
+        j = json.loads(r.text)
+        print(j)
